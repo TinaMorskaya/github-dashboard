46739a91b6c6092421485a5bcbbc504a8ca551c3
Daria Astapenko <ms.daria.astapenko@gmail.com>
2020-08-21 14:28:47 +0300

    migrate getting list repos in repos_container

--- src/main_page_components/Repos_Container.js
+++ src/main_page_components/Repos_Container.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "../App.css";
 import {RepositoryEntry} from "./Repository_Entry.js";
+import {getListTopRepos} from "../helpers/function/get_List_Top_Repos.js";
+import {getURL} from "../helpers/function/get_URL.js";
 export {ReposContainer};
 
 const ReposContainer = (props) => {
-    if (!props.listRepos) return null;
+    
+    const [listRepos, setListRepos] = useState(null);
+
+    useEffect(()=> {
+        const fetchData = async () => {  
+            let [newListRepos, lastPageNum] = await getListTopRepos(getURL(props.searchSettings));
+            setListRepos(newListRepos);
+            if (props.lastPageNum !== lastPageNum) {
+                props.setLastPageNum(lastPageNum)
+            }
+        }
+        fetchData();
+    }, [props.searchSettings]);
+
+    if (!listRepos) return null;
     return (
     <div className='second-main-flex'>
-        {props.listRepos.map((repo) => 
+        {listRepos.map((repo) => 
             <RepositoryEntry 
                 key = {repo.get('name')}
                 repoName = {repo.get('name')} 
